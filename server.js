cc8dd30ef5a5d7950d2f0fba882ea037a753bcbc
Ricardo J González C <rjgcastillo@hotmail.com>
2020-12-02 10:54:15 -0600

    First refactoring in server

--- server.js
+++ server.js
@@ -4,307 +4,308 @@ const io = require('socket.io')(http)
 
 const {Room} = require('./models/room.js')
 const {TreasureList, DoorList} = require('./models/cardLists.js')
-const room = require('./models/room.js')
 
 let rooms = []
 
 // Socket IO
 io.on('connection', (socket) => {
-	console.log('a user connected ' + socket.id)
-	
-	/*======================ROOM  MANAGEMENT=======================*/
-    socket.on('createRoom', (roomName) => {
-        if (!(/[^\w.]/.test(roomName))) {
-            socket.join(roomName, () => {
-                console.log(`User ${socket.id} connected to room ${roomName}`)
-			});
-			let newRoom = new Room(roomName, socket.id)
-			newRoom.addPlayer(socket.id)
-			rooms.push(newRoom)
-        }
-	})
-	
-	socket.on('joinRoom', (roomName) => {
-		if (!(/[^\w.]/.test(roomName))) {
-			socket.join(roomName, () => {
-                console.log(`User ${socket.id} connected to room ${roomName}`)
-			});
-			let roomIndex = findRoom(roomName)
-			rooms[roomIndex].addPlayer(socket.id)
-			socket.emit('updateTokenSelections', rooms[roomIndex].availableTokens)
-		}
-	})
+  console.log(`A user connected: {socked.id}`)
 
-	/*======================LOBBY UPDATES=======================*/
-	socket.on('joined', (roomName) => {
-		let roomIndex = findRoom(roomName)
-		socket.emit('updateTokenSelections', rooms[roomIndex].availableTokens)
-	})
+  const tryGetRoom = (roomName) => {
+    const roomIndex = findRoom(roomName)
+    if (roomIndex < 0) {
+      console.log("Error: room doesn't exist")
+      return null;
+    }
+    return rooms[roomIndex];
+  }
 
-	socket.on('selectAttribute', (roomName, type, value) => {
-		let roomIndex = findRoom(roomName)
-		if (roomIndex < 0) {
-			console.log("Error: room doesn't exist")
-			return
-		}
+  /*======================ROOM  MANAGEMENT=======================*/
+  const notifyJoiningRoom = (roomName) => () => {
+    console.log(`User ${socket.id} connected to room ${roomName}`)
+  };
 
-		let playerIndex = findPlayer(rooms[roomIndex], socket.id)
-		if (playerIndex < 0) {
-			console.log("Error: player not found")
-			return
-		}
+  socket.on('createRoom', (roomName) => {
+    if (!(/[^\w.]/.test(roomName))) {
+      socket.join(roomName, notifyJoiningRoom(roomName));
+      let newRoom = new Room(roomName, socket.id)
+      newRoom.addPlayer(socket.id)
+      rooms.push(newRoom)
+    }
+  })
 
-		if (type === 'token') {
-			if(rooms[roomIndex].players[playerIndex].tokenImage != "") {
-				rooms[roomIndex].availableTokens.push(rooms[roomIndex].players[playerIndex].tokenImage)
-			}
+  socket.on('joinRoom', (roomName) => {
+    if (!(/[^\w.]/.test(roomName))) {
+      socket.join(roomName, notifyJoiningRoom(roomName));
+      let roomIndex = findRoom(roomName)
+      rooms[roomIndex].addPlayer(socket.id)
+      socket.emit('updateTokenSelections', rooms[roomIndex].availableTokens)
+    }
+  })
 
-			rooms[roomIndex].players[playerIndex].tokenImage = value
-			rooms[roomIndex].availableTokens = rooms[roomIndex].availableTokens.filter(token => {
-				return token != value
-			})
+  /*======================LOBBY UPDATES=======================*/
+  socket.on('joined', (roomName) => {
+    let roomIndex = findRoom(roomName)
+    socket.emit('updateTokenSelections', rooms[roomIndex].availableTokens)
+  })
 
-			io.in(rooms[roomIndex].name).emit('updateTokenSelections', rooms[roomIndex].availableTokens)
-		} else if (type === 'gender'){
-			rooms[roomIndex].players[playerIndex].gender = value
-		} else {
-			console.log("Error: unexpected attribute")
-		}
-	})
+  socket.on('selectAttribute', (roomName, type, value) => {
+    const room = tryGetRoom(roomName);
+    if (room == null) return;
 
-	/*======================GAME MANAGEMENT=======================*/
-	socket.on('startGame', (roomName) => {
-		let roomIndex = findRoom(roomName)
-		if (roomIndex >= 0) {
-			if (socket.id == rooms[roomIndex].hostId) {
-				rooms[roomIndex].shuffleDecks(TreasureList, DoorList)
-				io.in(roomName).emit('startGame', rooms[roomIndex].getInfo())
-			}
-		} else {
-			console.log("Error: invalid room name")
-		}
-	})
+    const playerIndex = findPlayer(room, socket.id)
+    if (playerIndex < 0) {
+      console.log('Error: player not found')
+      return
+    }
 
-	socket.on('endPregame', (roomName) => {
-		let roomIndex = findRoom(roomName)
-		if (roomIndex < 0) {
-			console.log("Error: room doesn't exist")
-			return
-		}
+    const player = room.players[playerIndex]
+    if (type === 'token') {
+      if(player.tokenImage != '') {
+        room.availableTokens.push(player.tokenImage)
+      }
 
-		let playerIndex = findPlayer(rooms[roomIndex], socket.id)
-		if (playerIndex < 0) {
-			console.log("Error: player not found")
-			return
-		}
+      player.tokenImage = value
+      room.availableTokens = room.availableTokens.filter(token => token != value)
 
-		if (rooms[roomIndex].players[playerIndex].inPregame) {
-			rooms[roomIndex].players[playerIndex].inPregame = false
-			if (checkPregame(rooms[roomIndex].players)) {
-				// Someone is still in pregame, do nothing
-			} else {
-				rooms[roomIndex].shufflePlayers()
-				let nextId = rooms[roomIndex].getNextPlayerId()
-				io.in(roomName).emit('endPregame')
-				io.in(roomName).emit('changeTurn', nextId)
-			}
-		}
-	})
+      io.in(room.name).emit('updateTokenSelections', room.availableTokens)
+    } else if (type === 'gender') {
+      player.gender = value
+    } else {
+      console.log('Error: unexpected attribute')
+    }
+  })
 
-	socket.on('drewCard', (roomName) => {
-		io.in(roomName).emit('drewCard')
-	})
+  /*======================GAME MANAGEMENT=======================*/
+  socket.on('startGame', (roomName) => {
+    let roomIndex = findRoom(roomName)
+    if (roomIndex >= 0) {
+      if (socket.id == rooms[roomIndex].hostId) {
+        rooms[roomIndex].shuffleDecks(TreasureList, DoorList)
+        io.in(roomName).emit('startGame', rooms[roomIndex].getInfo())
+      }
+    } else {
+      console.log("Error: invalid room name")
+    }
+  })
 
-	socket.on('endTurn', (roomName) => {
-		let roomIndex = findRoom(roomName)
-		if (roomIndex < 0) {
-			console.log("Error: room doesn't exist")
-			return
-		}
+  socket.on('endPregame', (roomName) => {
+    const room = tryGetRoom(roomName)
+    if (room == null) {
+      return
+    }
 
-		let nextId = rooms[roomIndex].getNextPlayerId()
-		io.in(roomName).emit('changeTurn', nextId)
-	})
+    let playerIndex = findPlayer(room, socket.id)
+    if (playerIndex < 0) {
+      console.log("Error: player not found")
+      return
+    }
 
-	socket.on('winGame', (roomName) => {
-		io.in(roomName).emit('endGame', socket.id)
-	})
+    if (room.players[playerIndex].inPregame) {
+      room.players[playerIndex].inPregame = false
+      if (checkPregame(room.players)) {
+        // Someone is still in pregame, do nothing
+      } else {
+        room.shufflePlayers()
+        let nextId = room.getNextPlayerId()
+        io.in(roomName).emit('endPregame')
+        io.in(roomName).emit('changeTurn', nextId)
+      }
+    }
+  })
 
-	/*======================TOKEN MOVEMENT=======================*/
-	socket.on('moveToken', (roomName, x, y) => {
-		socket.to(roomName).emit('moveOpponentToken', socket.id, x, y);
-	})
+  socket.on('drewCard', (roomName) => {
+    io.in(roomName).emit('drewCard')
+  })
 
-	/*======================CARD HANDLING=======================*/
-	socket.on('requestCards', (roomName, cardType, n) => {
-		let roomIndex = findRoom(roomName)
-		if (roomIndex < 0) {
-			console.log("Error: room doesn't exist")
-			return
-		}
+  socket.on('endTurn', (roomName) => {
+    const room = tryGetRoom(roomName)
+    if (room == null) {
+      return
+    }
 
-		let playerIndex = findPlayer(rooms[roomIndex], socket.id)
-		if (playerIndex < 0) {
-			console.log("Error: player not found")
-			return
-		}
+    let nextId = room.getNextPlayerId()
+    io.in(roomName).emit('changeTurn', nextId)
+  })
 
-		if (cardType !== 'treasure' && cardType !== 'door') {
-			console.log("Error: unexpected card type")			
-			return
-		}
+  socket.on('winGame', (roomName) => {
+    io.in(roomName).emit('endGame', socket.id)
+  })
 
-		let response = []
-		for (let i = 0; i < n; i++) {
-			rooms[roomIndex].players[playerIndex].cards.push(cardType)
-			if (cardType === 'treasure') {
-				response.push(rooms[roomIndex].treasureDeck.pop())
-			} else {
-				response.push(rooms[roomIndex].doorDeck.pop())
-			}
-		}
+  /*======================TOKEN MOVEMENT=======================*/
+  socket.on('moveToken', (roomName, x, y) => {
+    socket.to(roomName).emit('moveOpponentToken', socket.id, x, y);
+  })
 
-		socket.emit('addCardsToPlayer', response, cardType)
-		socket.to(roomName).emit('updateOpponentCards', socket.id, rooms[roomIndex].players[playerIndex].cards)
-	})
+  /*======================CARD HANDLING=======================*/
+  socket.on('requestCards', (roomName, cardType, n) => {
+    const room = tryGetRoom(roomName)
+    if (room == null) {
+      return
+    }
 
-	socket.on('distributeCards', roomName => {
-		let roomIndex = findRoom(roomName)
-		if (roomIndex < 0) {
-			console.log("Error: room doesn't exist")
-			return
-		}
+    let playerIndex = findPlayer(room, socket.id)
+    if (playerIndex < 0) {
+      console.log("Error: player not found")
+      return
+    }
 
-		let playerIndex = findPlayer(rooms[roomIndex], socket.id)
-		if (playerIndex < 0) {
-			console.log("Error: player not found")
-			return
-		}
+    if (cardType !== 'treasure' && cardType !== 'door') {
+      console.log("Error: unexpected card type")
+      return
+    }
 
-		let doors = []
-		let treasures = []
-		for (let i = 0; i < 4; i++) {
-			treasures.push(rooms[roomIndex].treasureDeck.pop())
-			rooms[roomIndex].players[playerIndex].cards.push('treasure')
-		}
-		for (let i = 0; i < 4; i++) {
-			doors.push(rooms[roomIndex].doorDeck.pop())
-			rooms[roomIndex].players[playerIndex].cards.push('door')
-		}
-		
-		socket.emit('distributeCards', treasures, doors)
-		socket.to(roomName).emit('updateOpponentCards', socket.id, rooms[roomIndex].players[playerIndex].cards)
-	})
+    let response = []
+    for (let i = 0; i < n; i++) {
+      room.players[playerIndex].cards.push(cardType)
+      if (cardType === 'treasure') {
+        response.push(room.treasureDeck.pop())
+      } else {
+        response.push(room.doorDeck.pop())
+      }
+    }
 
-	socket.on('removeCard', (roomName, cardName, cardType, index) => {
-		let roomIndex = findRoom(roomName)
-		if (roomIndex < 0) {
-			console.log("Error: room doesn't exist")
-			return
-		}
+    socket.emit('addCardsToPlayer', response, cardType)
+    socket.to(roomName).emit('updateOpponentCards', socket.id, room.players[playerIndex].cards)
+  })
 
-		let playerIndex = findPlayer(rooms[roomIndex], socket.id)
-		if (playerIndex < 0) {
-			console.log("Error: player not found")
-			return
-		}
-		
-		if (cardType === 'treasure') {
-			rooms[roomIndex].treasureDeck.push(cardName)
-		} else {
-			rooms[roomIndex].doorDeck.push(cardName)
-		}
-		
-		rooms[roomIndex].players[playerIndex].removeCardAt(index)
-		socket.to(roomName).emit('updateOpponentCards', socket.id, rooms[roomIndex].players[playerIndex].cards)
-	})
+  socket.on('distributeCards', roomName => {
+    let roomIndex = findRoom(roomName)
+    if (roomIndex < 0) {
+      console.log("Error: room doesn't exist")
+      return
+    }
 
-	/*======================PLAYER UPDATES=======================*/
-	socket.on('updateLevel', (roomName, socketId, level) => {
-		let roomIndex = findRoom(roomName)
-		if (roomIndex < 0) {
-			console.log("Error: room doesn't exist")
-			return
-		}
+    let playerIndex = findPlayer(rooms[roomIndex], socket.id)
+    if (playerIndex < 0) {
+      console.log("Error: player not found")
+      return
+    }
 
-		let playerIndex = findPlayer(rooms[roomIndex], socketId)
-		if (playerIndex < 0) {
-			console.log("Error: player not found")
-			return
-		}
+    let doors = []
+    let treasures = []
+    for (let i = 0; i < 4; i++) {
+      treasures.push(rooms[roomIndex].treasureDeck.pop())
+      rooms[roomIndex].players[playerIndex].cards.push('treasure')
+    }
+    for (let i = 0; i < 4; i++) {
+      doors.push(rooms[roomIndex].doorDeck.pop())
+      rooms[roomIndex].players[playerIndex].cards.push('door')
+    }
 
-		rooms[roomIndex].players[playerIndex].level = level
-		socket.to(roomName).emit('updateLevel', socketId, rooms[roomIndex].players[playerIndex].level)
-	})
+    socket.emit('distributeCards', treasures, doors)
+    socket.to(roomName).emit('updateOpponentCards', socket.id, rooms[roomIndex].players[playerIndex].cards)
+  })
 
-	/*======================PLAYER DISCONNECT=======================*/
-	socket.on('disconnect', () => {
-        console.log('a user disconnected ' + socket.id)
-    })
+  socket.on('removeCard', (roomName, cardName, cardType, index) => {
+    let roomIndex = findRoom(roomName)
+    if (roomIndex < 0) {
+      console.log("Error: room doesn't exist")
+      return
+    }
+
+    let playerIndex = findPlayer(rooms[roomIndex], socket.id)
+    if (playerIndex < 0) {
+      console.log("Error: player not found")
+      return
+    }
+
+    if (cardType === 'treasure') {
+      rooms[roomIndex].treasureDeck.push(cardName)
+    } else {
+      rooms[roomIndex].doorDeck.push(cardName)
+    }
+
+    rooms[roomIndex].players[playerIndex].removeCardAt(index)
+    socket.to(roomName).emit('updateOpponentCards', socket.id, rooms[roomIndex].players[playerIndex].cards)
+  })
+
+  /*======================PLAYER UPDATES=======================*/
+  socket.on('updateLevel', (roomName, socketId, level) => {
+    let roomIndex = findRoom(roomName)
+    if (roomIndex < 0) {
+      console.log("Error: room doesn't exist")
+      return
+    }
+
+    let playerIndex = findPlayer(rooms[roomIndex], socketId)
+    if (playerIndex < 0) {
+      console.log("Error: player not found")
+      return
+    }
+
+    rooms[roomIndex].players[playerIndex].level = level
+    socket.to(roomName).emit('updateLevel', socketId, rooms[roomIndex].players[playerIndex].level)
+  })
+
+  /*======================PLAYER DISCONNECT=======================*/
+  socket.on('disconnect', () => {
+    console.log('a user disconnected ' + socket.id)
+  })
 })
 
 
 // Server
 server.get("/api/roomExists", (req, res, next) => {
-    let name = req.query.name
-	res.header('Access-Control-Allow-Origin', '*')
-	
-    return res.status(200).json({ans: findRoom(name) >=0})
+  let name = req.query.name
+  res.header('Access-Control-Allow-Origin', '*')
+
+  return res.status(200).json({ans: findRoom(name) >=0})
 })
 
 server.get("/api/roomIsJoinable", (req, res, next) => {
-    let name = req.query.name
-	res.header('Access-Control-Allow-Origin', '*')
+  let name = req.query.name
+  res.header('Access-Control-Allow-Origin', '*')
 
-	let roomIndex = findRoom(name)
-	let ans = false
-	let message = ""
+  let roomIndex = findRoom(name)
+  let ans = false
+  let message = ""
 
-	if (roomIndex >= 0) {
-		if (rooms[roomIndex].players.length < 4) {
-			ans = true
-		} else {
-			message = "Room exists but is full."
-		}
-	} else {
-		message = "Room doesn't exist."
-	}
-	
-    return res.status(200).json({ans: ans, message: message})
+  if (roomIndex >= 0) {
+    if (rooms[roomIndex].players.length < 4) {
+      ans = true
+    } else {
+      message = "Room exists but is full."
+    }
+  } else {
+    message = "Room doesn't exist."
+  }
+
+  return res.status(200).json({ans: ans, message: message})
 })
 
 function findRoom(name) {
-    let ans = -1
-    rooms.forEach((room, i) => {
-		if (room.name === name) {
-			ans = i
-		}
-	})
+  let ans = -1
+  rooms.forEach((room, i) => {
+    if (room.name === name) {
+      ans = i
+    }
+  })
 
-	return ans
+  return ans
 }
 
 function findPlayer(room, socketId) {
-	let ans = -1
-	room.players.forEach((player, i) => {
-		if (player.socketId == socketId) {
-			ans = i
-		}
-	})
+  let ans = -1
+  room.players.forEach((player, i) => {
+    if (player.socketId == socketId) {
+      ans = i
+    }
+  })
 
-	return ans
+  return ans
 }
 
 function checkPregame(players) {
-	for(let i = 0; i < players.length; i++) {
-		if(players[i].inPregame) {
-			return true
-		}
-	}
+  for(let i = 0; i < players.length; i++) {
+    if(players[i].inPregame) {
+      return true
+    }
+  }
 
-	return false
+  return false
 }
 
 http.listen(3000, () => {
-    console.log('Server started!')
+  console.log('Server started!')
 })
